6c818186e21a62f9c44fff85c07ded639c2b84b7
kianah10 <kharri10@chicagobooth.edu>
2023-01-25 04:33:56 +0000

    square/result good

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
 def blank_square_form
 
     render({ :template => "calculation_templates/square_form.html.erb"})
+end
 
+def calculate_square
+#params =
+
+@num = params.fetch("user").to_f
+@square = @num ** 2
+  render({ :template => "calculation_templates/square_results.html.erb"})
   end
 end
